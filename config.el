baf2286cf6cf0c96a08b16374c8112e09c6f1494
Fxzz1x2 <kareemabukush@gmail.com>
2024-10-14 21:12:54 +0200

    added php/vue lsp + icons

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dark+)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -74,4 +74,9 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; vue 3 lsp
 (add-hook 'vue-mode-hook #'lsp!)
+
+;; relative number
+(setq display-line-numbers-type 'relative)
---
+++ custom.el
@@ -0,0 +1,21 @@
+(put 'customize-face 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1e1e1e" :foreground "#e4e4e4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
+ '(mmm-default-submode-face ((t nil)))
+ '(web-mode-block-face ((t (:background nil))))
+ '(web-mode-current-element-highlight-face ((t (:background nil))))
+ '(web-mode-html-attr-name-face ((t (:background nil))))
+ '(web-mode-html-tag-face ((t (:background nil))))
+ '(web-mode-part-face ((t (:background nil))))
+ '(web-mode-script-face ((t (:background nil))))
+ '(web-mode-style-face ((t (:background nil)))))
